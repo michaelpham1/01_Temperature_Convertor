430de925a36d3ae4cbc0701d18328740cc78d4fe
michaelpham1 <51727137+michaelpham1@users.noreply.github.com>
2021-02-18 11:51:24 +1300

    Partial help gui

---
+++ 01_Help_GUI.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables...
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=600, height=600, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+                                          font=("Arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+        # Help Button (row 1)
+        self.help_button = Button(self.converter_frame, text="Help",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10, command=self.help)
+        self.help_button.grid(row=1)
+
+    def help(self):
+        print("You asked for help")
+        get_help = Help(self)
+        get_help.help_text.configure(text="Help text goes here")
+
+
+class Help:
+    def __init__(self, partner):
+
+        background = "orange"
+
+        # disable help button
+        partner.help.button.config(state=DISABLED)
+
+                # Sets up child window (ie: help box)
+        self.help_box = Toplevel()
+
+        # Set up GUI Frame
+        self.help_frame = Frame(self.help_box, bg=background)
+        self.help_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.how_heading = Label(self.help_frame, text="Help / Instructions",
+                                 )
+
+        # Help text(label, row 1)
+
+        # Dismiss button (row 2)
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    something = Converter()
+    root.mainloop()
