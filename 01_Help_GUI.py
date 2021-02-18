8648a65c513260ca9d80cec5dadceff825aa1389
michaelpham1 <51727137+michaelpham1@users.noreply.github.com>
2021-02-19 10:11:29 +1300

    Update 01_Help_GUI.py
    HELP GUI FINAL

--- 01_Help_GUI.py
+++ 01_Help_GUI.py
@@ -40,22 +40,38 @@ class Help:
         background = "orange"
 
         # disable help button
-        partner.help.button.config(state=DISABLED)
+        partner.help_button.config(state=DISABLED)
 
-                # Sets up child window (ie: help box)
+        # Sets up child window (ie: help box)
         self.help_box = Toplevel()
 
+        # If users press cross at top, closes help and 'release' help button
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
         # Set up GUI Frame
-        self.help_frame = Frame(self.help_box, bg=background)
+        self.help_frame = Frame(self.help_box, width=300, bg=background)
         self.help_frame.grid()
 
         # Set up Help heading (row 0)
         self.how_heading = Label(self.help_frame, text="Help / Instructions",
-                                 )
+                                 font="arial 14 bold", bg=background)
+        self.how_heading.grid(row=0)
 
         # Help text(label, row 1)
+        self.help_text = Label(self.help_frame, text="",
+                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.help_text.grid(row=1)
 
         # Dismiss button (row 2)
+        self.dismiss_btn = Button(self.help_frame, text="Dismiss",
+                                  width=10, bg="orange", font="arial 10 bold",
+                                  command=partial(self.close_help, partner))
+        self.dismiss_btn.grid(row=2, pady=10)
+
+    def close_help(self, partner):
+        # Put help button back to normal...
+        partner.help_button.config(state=NORMAL)
+        self.help_box.destroy()
 
 
 
