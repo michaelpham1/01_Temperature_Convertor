2a83d176c974325bc4207117a89598c4410be840
michaelpham1 <51727137+michaelpham1@users.noreply.github.com>
2021-02-25 12:08:15 +1300

    i lost everything :(

---
+++ 04_f_to_c.py
@@ -0,0 +1,19 @@
+# quick component to convert degrees F to C.
+# Function takes in value, does conversion and puts answer into a list
+
+
+def to_c(from_f):
+    celsius = (from_f - 32) * 5/9
+    return celsius
+
+
+# Main Routine
+temperatures = [0, 40, 100]
+converted = []
+
+for item in temperatures:
+    answer = to_c(item)
+    ans_statement = "{} degrees in F is {} degrees C".format(item, answer)
+    converted.append(ans_statement)
+
+print(converted)
